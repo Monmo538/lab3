77a44ba0bbc0ffcd3b4411ce827c727de0a8d37d
adeel-ahmad-awan <destontrace@gmail.com>
2022-09-15 22:18:47 +0200

    add dataset in package

---
+++ R/wiki_graph.R
@@ -0,0 +1,8 @@
+#' Wiki_graph data frame as a dataset
+#'
+#' @name wiki_graph
+#' @docType data
+#' @author Adeel \email{adeah090@@student.liu.se}
+#' @references \url{https://en.wikipedia.org/wiki/File:Wiki_graph.jpg}
+#' @keywords data
+NULL
